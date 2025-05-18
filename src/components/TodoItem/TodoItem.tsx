9709006a859afb08db0cfd5f6f0f672eb554db4b
Illia <hetsko.ilya@gmail.com>
2025-05-19 01:23:01 +0200

    fix task solution

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -31,8 +31,10 @@ export const TodoItem: React.FC<Props> = ({ todo, isEditing, isLoading }) => {
         </form>
       ) : (
         <>
-          <span className="todo__title">{todo.title}</span>
-          <button data-cy="DeleteButton" type="button" className="todo__remove">
+          <span data-cy="TodoTitle" className="todo__title">
+            {todo.title}
+          </span>
+          <button data-cy="TodoDelete" type="button" className="todo__remove">
             ×
           </button>
         </>
